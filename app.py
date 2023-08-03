96a73815762c5d9627746f839577bea0cb2ad04d
SILVA Alonso <alonso.silva@gmail.com>
2023-08-03 09:10:51 +0200

    Add app

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from thispersondoesnotexist import get_online_person, save_picture
+
+with st.sidebar:
+    st.title("Echo Bot + ThisPersonDoesNotExist")
+    st.image("a_beautiful_person.jpeg", width=200)
+    if st.button("Generate a new Image"):
+        picture = get_online_person()
+        save_picture(picture, "a_beautiful_person.jpeg")
+        st.experimental_rerun()
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
